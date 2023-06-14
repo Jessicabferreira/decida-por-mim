f6c142d79f56f247801beb69a2c3e32b2d0128b7
Jessicabferreira <jessiberteloni@gmail.com>
2023-06-14 18:25:46 -0300

    Initial commit

---
+++ decida_por_mim/decida_por_mim.py
@@ -0,0 +1,17 @@
+import random
+
+class DecidaPorMim:
+    def __init__(self):
+        self.respostas = [
+            'Com certeza, você deve fazer isso',
+            'Não sei, você que sabe',
+            'Não faz isso não',
+            'Acho que tá na hora certa!'
+        ]
+
+    def Iniciar(self):
+        input("Faça sua pergunta: ")
+        print(random.choice(self.respostas))
+
+decida = DecidaPorMim()
+decida.Iniciar()
